a13e210d7736c72752f9e0d9ddb34d80a4504abf
RenanLuca <renandelucamachado3232@gmail.com>
2022-03-30 21:06:23 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1> Calculadora </h1>
+  <Calculator/>
+  </>
+  ,
   document.getElementById('root')
 );
 
